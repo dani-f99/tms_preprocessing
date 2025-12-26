147f2d6986a9393c8405638f8eb38aa9ec393dcb
unknown <danielf3124@gmail.com>
2025-12-26 21:10:34 +0200

    finished packages verification functionality

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 # Imports
-from source.helpers import run_pipeline
+from source.helpers import run_pipeline, check_packages
 from source.pipeline1_preprocessing import PipelinePreprocessingTest
 from source.pipeline2_matrixmaker import PipelineMatrixMakerTest
+import pandas as pd
+
+# Cheeking that the required python packages are installed
+installed_packages = check_packages(["pandas", "numpy", "scipy", "tqdm", "pandarallel", "mysql-connector-python"])
+missing = installed_packages.loc[installed_packages.not_installed == 1, "not_installed"].index.to_list()
+
+# If some missing packages exists -> raise error
+if len(missing) > 0:
+    raise Exception(f"\nplease install required packages: {missing}")
 
 # Execute the pipeline
 if __name__ == "__main__":
