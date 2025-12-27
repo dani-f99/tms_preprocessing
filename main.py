06ec1c53dc61933b676a2c979db6e4c1653c810e
dani-f99 <danielf3124@gmail.com>
2025-12-27 10:53:48 +0200

    fixed bug in folers paths

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # Imports
-from source.helpers import run_pipeline, check_packages
+from source.helpers import run_pipeline, check_packages, create_folders
 from source.pipeline1_preprocessing import PipelinePreprocessingTest
 from source.pipeline2_matrixmaker import PipelineMatrixMakerTest
 import pandas as pd
@@ -11,8 +11,11 @@ missing = installed_packages.loc[installed_packages.not_installed == 1, "not_ins
 # If some missing packages exists -> raise error
 if len(missing) > 0:
     raise Exception(f"\nplease install required packages: {missing}")
+    
+# Create required folders -> the input is the built in argument input, it's written here for clearity
+create_folders(["temp_data", "tms_input", "reports"])
 
 # Execute the pipeline
 if __name__ == "__main__":
     pipeline1_result = run_pipeline(PipelinePreprocessingTest, pipeline_name="preprocessing_pipeline1")
-    pipeline2_results = run_pipeline(PipelineMatrixMakerTest, pipeline_name="matrixmaker_pipeline2")
+    #pipeline2_results = run_pipeline(PipelineMatrixMakerTest, pipeline_name="matrixmaker_pipeline2")
